41ddfd365590a7bedd3e3838c6ab53f73443d39c
Eric Benzacar <eric.benzacar@wada-ama.org>
2021-09-30 16:09:21 -0400

    Add custom state manager to ensure that the SavedImage is always updated when unloaded fields are loaded

--- src/main/java/mydomain/datanucleus/ExtendedReferentialStateManagerImpl.java
+++ src/main/java/mydomain/datanucleus/ExtendedReferentialStateManagerImpl.java
@@ -1,2 +1,51 @@
-package mydomain.datanucleus;public class ExtendedReferentialStateManagerImpl {
+package mydomain.datanucleus;
+
+import org.datanucleus.ExecutionContext;
+import org.datanucleus.enhancement.Persistable;
+import org.datanucleus.metadata.AbstractClassMetaData;
+import org.datanucleus.state.ReferentialStateManagerImpl;
+import org.datanucleus.util.ClassUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * Extended state manager used to provide access to the SavedImage of the Persistable instance
+ * and to update the SavedImage when unloaded fields are loaded
+ */
+
+
+// ObjectProvider.ORIGINAL_FIELD_VALUE_KEY_PREFIX
+
+public class ExtendedReferentialStateManagerImpl extends ReferentialStateManagerImpl {
+    public ExtendedReferentialStateManagerImpl(ExecutionContext ec, AbstractClassMetaData cmd) {
+        super(ec, cmd);
+    }
+
+    public Persistable getSavedImage(){
+        return savedImage;
+    }
+
+    @Override
+    public void loadUnloadedFields() {
+        super.loadUnloadedFields();
+
+        // if there is a saved image, ensure that all missing fields are now made available in the saved image as well
+        if( savedLoadedFields == null )
+            return;
+
+        // identify all the fields left to copy
+        int[] fieldsToCopy = ClassUtils.getFlagsSetTo(savedLoadedFields, cmd.getAllMemberPositions(), false);
+        if( fieldsToCopy == null || fieldsToCopy.length == 0){
+            return;
+        }
+
+        savedImage.dnCopyFields(myPC, fieldsToCopy);
+        savedLoadedFields = loadedFields.clone();
+    }
+
+    public Object provideSavedField(int fieldNumber) {
+        return provideField(savedImage, fieldNumber);
+    }
 }
