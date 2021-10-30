c66ce4f64e1efe0657217e6dfd74194475bd1b10
Eric Benzacar <eric.benzacar@wada-ama.org>
2021-10-29 22:16:31 -0400

    Add test to show that embedded entities are still showing up as REF items but not included as a primary entity

--- src/main/java/mydomain/model/Teacher.java
+++ src/main/java/mydomain/model/Teacher.java
@@ -4,12 +4,18 @@ import mydomain.audit.DataTrail;
 
 import javax.jdo.annotations.Inheritance;
 import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
 
 
 @DataTrail
 @Inheritance(strategy= InheritanceStrategy.SUPERCLASS_TABLE)
+@PersistenceCapable
 public class Teacher extends BaseTeacher{
-    public Teacher(String name) {
+
+    private BaseTeacher mentor;
+
+    public Teacher(String name, BaseTeacher mentor) {
         super(name);
+        this.mentor = mentor;
     }
 }
--- src/test/java/org/datanucleus/test/DatatrailExclusionTest.java
+++ src/test/java/org/datanucleus/test/DatatrailExclusionTest.java
@@ -1,11 +1,18 @@
 package org.datanucleus.test;
 
+import com.spotify.hamcrest.pojo.IsPojo;
+import mydomain.datanucleus.datatrail.Node;
+import mydomain.datanucleus.datatrail.NodeType;
 import mydomain.model.BaseTeacher;
+import mydomain.model.Teacher;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
 
 public class DatatrailExclusionTest extends AbstractTest{
 
@@ -17,5 +24,35 @@ public class DatatrailExclusionTest extends AbstractTest{
         });
 
         assertThat("No entities in audit trail", audit.getModifications(), Matchers.hasSize(0));
+    }
+
+    @Test
+    public void childTeacherIncluded(){
+        executeTx(pm -> {
+            Teacher teacher = new Teacher("teacher", null);
+            pm.makePersistent(teacher);
+        });
+
+        IsPojo<Node> teacher = getEntity(Node.Action.CREATE, Teacher.class, "1")
+                .withProperty("fields",  hasItem(
+                        getField(NodeType.PRIMITIVE, String.class, "name", "teacher", null)
+                ));
+        assertThat(audit.getModifications(), contains(teacher));
+    }
+
+    @Test
+    public void embededBaseTeacherIncluded(){
+        executeTx(pm -> {
+            BaseTeacher mentor = new BaseTeacher("mentor");
+            Teacher teacher = new Teacher("teacher", mentor);
+            pm.makePersistent(teacher);
+        });
+
+        IsPojo<Node> teacher = getEntity(Node.Action.CREATE, Teacher.class, ANY )
+                .withProperty("fields",  hasItems(
+                        getField(NodeType.PRIMITIVE, String.class, "name", "teacher", null),
+                        getField(NodeType.REF, BaseTeacher.class, "mentor", ANY, null)
+                ));
+        assertThat(audit.getModifications(), contains(teacher));
     }
 }
