025e0429a904baf9f13b15de5a93608187fae81c
Eric Benzacar <eric.benzacar@wada-ama.org>
2021-11-02 22:22:53 -0400

    Identify the MapEntry as being updatable

--- src/main/java/mydomain/datanucleus/datatrail/nodes/MapEntry.java
+++ src/main/java/mydomain/datanucleus/datatrail/nodes/MapEntry.java
@@ -3,11 +3,13 @@ package mydomain.datanucleus.datatrail.nodes;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import mydomain.datanucleus.datatrail.Node;
 
+import java.util.Arrays;
+
 /**
  * Class to represent the key/value information found in the map.  Must not implement a Map.MapEntry as Jackson will automatically serialize those
  * differently
  */
-public class MapEntry extends BaseNode {
+public class MapEntry extends BaseNode implements Updatable{
     Node key;
     Node value;
 
@@ -28,5 +30,8 @@ public class MapEntry extends BaseNode {
         return value;
     }
 
-
+    @Override
+    public void updateFields() {
+        Arrays.asList(key, value).stream().filter(node -> node instanceof Updatable).forEach( node -> ((Updatable)node).updateFields());
+    }
 }
